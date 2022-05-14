1d7c4af5fb828d8674a76413798c21c08330ef66
amichno <95045327+amichno@users.noreply.github.com>
2022-05-14 21:28:26 +0200

    push -A

--- src/components/SectionUrl/SectionUrl.js
+++ src/components/SectionUrl/SectionUrl.js
@@ -1,7 +1,34 @@
-import styledComponents from "styled-components";
+import styled from "styled-components";
+
+const UrlWrapper = styled.div`
+    width:80%;
+    height: 7rem;
+    background-color: hsl(255, 11%, 22%);
+    background-image: url("../../images/bg-boost-desktop.svg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: bottom;
+    -webkit-border-radius: 5px;
+    -moz-border-radius: 5px;
+    border-radius: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Input = styled.input`
+    height: 3rem;
+    width: 60%;
+    padding-left: 0.5rem;
+`;
+
 
 const SectionUrl = ()=>{
-
+    return(
+        <UrlWrapper>
+            <Input type="text" placeholder="Shorten a link here..."></Input>
+        </UrlWrapper>
+    )
 }
 
 export default SectionUrl;
