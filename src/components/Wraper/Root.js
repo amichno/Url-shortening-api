2acd03e5677e8ebadf80e9d424cea7fcb993859c
amichno <95045327+amichno@users.noreply.github.com>
2022-05-19 12:15:03 +0200

    push -A

--- src/components/Wraper/Root.js
+++ src/components/Wraper/Root.js
@@ -1,56 +1,12 @@
 import React from "react";
-import styled from "styled-components";
 import SectionHeader from '../Header/sectionHeader';
 import Menu from "../Menu/menu";
 import { caption } from "../../Data/caption";
 import { description } from "../../Data/description";
 import SectionUrl from "../SectionUrl/SectionUrl";
 import SectionDescription from "../SectionDescription/sectionDescription";
-/* autoprefixer grid: autoplace */
+import { Wraper } from "./root.styles";
 
-const Wraper = styled.div`
-    
-    width: 100vw;
-    //max-width: 1440px;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    //margin: 1rem 2rem;
-    //margin-left: 2rem;
-   // padding-left: 2rem;
-    @media (max-width: 375px ){
-        width: 100vw;
-        height: 100%;
-    }
-    @media (min-width: 375px) and (max-width: 992px){
-        width: 90vw;
-       // max-width: 1440px;
-        height: 100%;
-        padding-left: 2rem;
-        padding-right: 2rem;
-        //margin-left: auto;
-        //margin-right: auto;
-    }
-    @media (min-width: 992px) and (max-width: 1440px) {
-        width: 90vw;
-        //max-width: 1440px;
-        height: 100%;
-        //margin: 1rem auto;
-        padding-left: 2rem;
-        padding-right: 2rem;
-        margin-left: auto;
-        margin-right: auto;
-    }
-
-    @media (min-width: 1440px){
-        max-width: 1440px;
-        height: 100%;
-        margin: 1rem auto;
-        padding-left: 1rem;
-        padding-right: 1rem;
-    }
-`;
 
 class Root extends React.Component{
 
